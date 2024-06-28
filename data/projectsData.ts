686bd6bf18851ef0c6ec748d866992fcafca90f7
Pedro Pinto <pedropinto@Pedros-MacBook-Pro.local>
2024-06-28 10:06:24 +0100

    +changes to the portfolio

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,21 +6,14 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    {
+    title: 'Efficient Vision Transformers',
+    description: `The project aimed to explore and optimize lightweight Vision Transformer (ViT) models specifically
+tailored for resource-constrained environments. The primary focus was on enhancing the efficiency
+and performance of these models in playing the Atari game Pong. The research involved developing and comparing three baseline ViT models against a primary
+, smaller ViT model. Key aspects of this comparison include model performance, memory footprint, and inference times.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/blog/Efficient-Vision-Transformers',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,18 +1,25 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Pedro Portfolio',
+  title: 'Pedro',
   author: 'Pedro Pinto',
-  headerTitle: 'Pedro',
+  headerTitle: 'Pedro Portfolio',
   description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteRepo: 'https://github.com/pedrocor12',
-	siteUrl: "https://pedrocor.vercel.app",
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
-  email: 'pintow.pedrow@gmail.com',
+  socialBanner: '/static/images/twitter-card.png',
+  mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'address@yoursite.com',
   github: 'https://github.com',
+  x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
+  threads: 'https://www.threads.net',
+  instagram: 'https://www.instagram.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
