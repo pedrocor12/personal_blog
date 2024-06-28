b7d51b868ff99f9cb4ac1c2ab9ffb0157a82c07c
pedrocor12 <pinto7652@gmail.com>
2024-06-28 17:34:27 +0100

    +theme color changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,19 +7,14 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/pedrocor12/personal_blog',
   siteLogo: '/static/images/test.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  email: 'pintow.pedrow@gmail.com',
+  github: 'https://github.com/pedrocor12',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
